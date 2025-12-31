9f61e4e56c8403616f23a84d00664e7aaba41c75
Asmit <ashmits554@gmail.com>
2026-01-01 01:01:35 +0530

    Refactor session store and update routes

--- index.js
+++ index.js
@@ -6,46 +6,45 @@ const session = require("express-session")
 const MongoStore = require("connect-mongo")
 
 const app = express()
-app.set("view engine", "hbs")
-app.use(express.static(path.join(__dirname, "public"))) // this line is  used to set path of public folder which contains cs, js and images
 
-// Parse URL-encoded bodies (for form POSTs like /admin/login)
+// View engine
+app.set("view engine", "hbs")
+
+// Static files
+app.use(express.static(path.join(__dirname, "public")))
+
+// Body parser (forms)
 app.use(express.urlencoded({ extended: false }))
-// If you also send JSON from clients, you can enable this too:
-// app.use(express.json())
 
 // Register partials
 hbs.registerPartials(path.join(__dirname, "views", "partials"))
-console.log("Partials registered");
-app.use(express.static("public")) // use this line to server public folder
 
+// Sessions
 app.use(session({
-    secret: process.env.SESSION_SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URL
-    }),
-    cookie: { secure: false }
-  }))
+  secret: process.env.SESSION_SECRET_KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI   // 
+  }),
+  cookie: { secure: false } 
+}))
 
-require("./helpers/index.js")
+// Helpers & DB
+require("./helpers")
 require("./db_connect")
 
-//MVC
+// Routes
 const router = require("./routes/routes")
-app.use("",router)
+app.use("/", router)
 
+const PORT = process.env.PORT || 8000
 
-app.listen(8000, () => {
-    console.log(`Server is running at http://localhost:8000`);
-}).on('error', (err) => {
-    console.error('Server failed to start:', err);
-});
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+}).on("error", (err) => {
+  console.error("Server failed to start:", err)
+})
 
 
 
@@ -53,35 +52,3 @@ app.listen(8000, () => {
 
 
 
-
-
-
-
-
-
-
-
-// app.get("/about",(req,res)=>{
-//     res.render("aboutPage", {title:"About US"})
-// })
-// app.get("/service",(req,res)=>{
-//     res.render("servicePage",{title:"Service"})
-// })
-// app.get("/features",(req,res)=>{
-//     res.render("featurePage",{title:"Features"})
-// })
-// app.get("/cars",(req,res)=>{
-//     res.render("carsPage",{title:"Cars"})
-// })
-// app.get("/team",(req,res)=>{
-//     res.render("teamPage",{title:"Team"})
-// })
-// app.get("/testimonials",(req,res)=>{
-//     res.render("testimonialPage",{title:"Testimonials"})
-// })
-// app.get("/contact",(req,res)=>{
-//     res.render("contact",{title:"Contact Us"})
-// })
-// app.use((req,res)=>{
-//     res.render("404", {title:"404! Page Not Found"})
-// })
