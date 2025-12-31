96c11461fb83854b6fde2f03942214d98f0e56e3
Asmit <ashmits554@gmail.com>
2026-01-01 01:10:21 +0530

    Change MongoDB connection URI environment variable

--- db_connect.js
+++ db_connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function getConnect() {
     try{
-       await mongoose.connect(process.env.MONGO_URI)
+       await mongoose.connect(process.env.MONGO_URL)
         console.log("Database is Connected")
     }catch(error) {
         console.log(error)
