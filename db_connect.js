a328dd7525caf09ea447bf61621f7d525e13cd39
Asmit <ashmits554@gmail.com>
2026-01-01 01:17:26 +0530

    Update MongoDB connection URI variable

--- db_connect.js
+++ db_connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function getConnect() {
     try{
-       await mongoose.connect(process.env.MONGO_URL)
+       await mongoose.connect(process.env.MONGO_URI)
         console.log("Database is Connected")
     }catch(error) {
         console.log(error)
